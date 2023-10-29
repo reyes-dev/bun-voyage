e5829ffe34ba2f413743b0d801d4f5d2aeb2669e
reyesdev <102765102+reyes-dev@users.noreply.github.com>
2023-10-29 02:22:16 -0700

    Add pictures to dragons display

--- src/views/list-dragons/list-dragons-ui.tsx
+++ src/views/list-dragons/list-dragons-ui.tsx
@@ -1,6 +1,7 @@
 export const ListDragonsUI = ({ dragons }) => {
   const dragonsList = dragons.map((dragon) => (
     <ul key={dragon.id}>
+      <img src={dragon.flickr_images[0]} width="250" />
       <li>{dragon.name}</li>
       <li>{dragon.type}</li>
       <li>{dragon.description}</li>
